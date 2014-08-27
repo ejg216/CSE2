c3fb5320afc0842c9d28a0c5cb6c68647289d7ab
ejg216 <ejg216@lehigh.edu>
2014-08-27 21:50:22 +0000

    updated WelcomeClass

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,29 @@
+///////////////////////////////////////////////////
+//Eric Glynn
+//Welcome Class Program
+
+//  first compile the program
+//      javac WelcomeClass.java
+//  run the program
+//      java WelcomeClass
+
+//  define a class
+public class WelcomeClass{
+    
+//  add main method
+  public static void main (String[] args) {
+      
+//  print the statement with signature and autobiographical blurb
+
+    System.out.println("  -----------");
+    System.out.println("  | WELCOME |");
+    System.out.println("  -----------");
+    System.out.println("  ^  ^  ^  ^  ^  ^");
+    System.out.println(" / \\/ \\/ \\/ \\/ \\/ \\"); 
+    System.out.println("|-E--J--G--2--1--6-|");
+    System.out.println(" \\ /\\ /\\ /\\ /\\ /\\ /");
+    System.out.println("  v  v  v  v  v  v");
+    System.out.println("This summer I went skydiving for the first time. It was very thrilling!");
+    
+  }
+}
