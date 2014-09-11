6ddf3f0b98ec9de4f70d16edaa3fa929bc3c3c30
ejg216 <ejg216@lehigh.edu>
2014-09-11 19:08:15 +0000

    updated Root

--- hw03/Root.java
+++ hw03/Root.java
@@ -12,20 +12,20 @@ public class Root {
 Scanner myScanner; //name the scanner
 myScanner = new Scanner ( System.in );
 
-System.out.println("Enter a double, and I print its cube root: ");
+System.out.println("Enter a double, and I print its cube root: "); //Prompt user to input a number
 double inputDigit = myScanner.nextDouble();
 
-double guessOne = inputDigit/3;
-double guessTwo = (2*guessOne*guessOne*guessOne+inputDigit)/(3*guessOne*guessOne);
-double guessThree = (2*guessTwo*guessTwo*guessTwo+inputDigit)/(3*guessTwo*guessTwo);
-double guessFour = (2*guessThree*guessThree*guessThree+inputDigit)/(3*guessThree*guessThree);
-double guessFive = (2*guessFour*guessFour*guessFour+inputDigit)/(3*guessFour*guessFour);
-double guessSix = (2*guessFive*guessFive*guessFive+inputDigit)/(3*guessFive*guessFive);
+double guessOne = inputDigit/3; //First guess of number
+double guessTwo = (2*guessOne*guessOne*guessOne+inputDigit)/(3*guessOne*guessOne); //Second guess of number
+double guessThree = (2*guessTwo*guessTwo*guessTwo+inputDigit)/(3*guessTwo*guessTwo); //Third guess of number
+double guessFour = (2*guessThree*guessThree*guessThree+inputDigit)/(3*guessThree*guessThree); //Fourth guess of number
+double guessFive = (2*guessFour*guessFour*guessFour+inputDigit)/(3*guessFour*guessFour); //Fifth guess of number
+double guessSix = (2*guessFive*guessFive*guessFive+inputDigit)/(3*guessFive*guessFive); //Sixth guess of number
 
-double digitGuess = (guessSix * guessSix * guessSix);
+double digitGuess = (guessSix * guessSix * guessSix); //Multiply the last guess by itself three times to find guess of cube
 
-System.out.println("The cube root is: " + guessSix + ".");
-System.out.println("My guess of your number based off this estimate is: " + digitGuess);
+System.out.println("The cube root is: " + guessSix + "."); //Print out the cube root guess
+System.out.println("My guess of your number based off this estimate is: " + digitGuess); //Print out original digit guess
 
 }
 
