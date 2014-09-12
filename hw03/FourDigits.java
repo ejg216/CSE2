7c4a13a2157f083c636ff93a0eee8c02263706db
ejg216 <ejg216@lehigh.edu>
2014-09-12 14:09:45 +0000

    updated FourDigits

--- hw03/FourDigits.java
+++ hw03/FourDigits.java
@@ -7,37 +7,37 @@
 import java.util.Scanner; //Import the scanner
 
 public class FourDigits {
-    public static void main(String[] args) { 
+    public static void main(String[] args) { //declare the method
 
 Scanner myScanner; //Name the scanner
 myScanner = new Scanner ( System.in );
 
-System.out.println("Enter a double and I display the four digits to the right of the decimal point: ");
+System.out.println("Enter a double and I display the four digits to the right of the decimal point: "); //prompt user for input
 double inputDigit = myScanner.nextDouble();
 
-double multipliedDigit = inputDigit * 10000;
-int roundedDigit = (int)multipliedDigit;
+double multipliedDigit = inputDigit * 10000; //multiply digit by 10000
+int roundedDigit = (int)multipliedDigit; //make this digit an integer
 
-int fourthDigit = (roundedDigit %10);
+int fourthDigit = (roundedDigit %10); //extract the fourth digit
 
+ 
+double nextMultipliedDigit = inputDigit * 1000; //multipy digit by 1000 to get last three digits
+int roundedNewDigit = (int)nextMultipliedDigit; //make digit an integer
 
-double nextMultipliedDigit = inputDigit * 1000;
-int roundedNewDigit = (int)nextMultipliedDigit;
-
-int thirdDigit = (roundedNewDigit %10);
-
-
-
-double newMultipliedDigit = inputDigit * 100;
-int roundedNewerDigit = (int)newMultipliedDigit;
-
-int secondDigit = (roundedNewerDigit %10);
+int thirdDigit = (roundedNewDigit %10); //extract third digit
 
 
-double firstMultipliedDigit = inputDigit * 10;
-int roundedNewestDigit = (int)firstMultipliedDigit;
 
-int firstDigit = (roundedNewestDigit %10);
+double newMultipliedDigit = inputDigit * 100; //multiply digit by 100 to get last two digits
+int roundedNewerDigit = (int)newMultipliedDigit; //make digit an integer
+ 
+int secondDigit = (roundedNewerDigit %10); //extract second digit
+
+
+double firstMultipliedDigit = inputDigit * 10; //multiply digit by 10 to get last digit
+int roundedNewestDigit = (int)firstMultipliedDigit; //make digit an integer
+
+int firstDigit = (roundedNewestDigit %10); //extract first digit
 
 System.out.println("The four digits are: " +firstDigit + secondDigit + thirdDigit + fourthDigit);
 
